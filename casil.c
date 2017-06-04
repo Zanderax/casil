bfa022d111a0ca282dc00773973536fc36d6588b
David <davidjamesbelcher0@gmail.com>
2017-06-04 00:19:21 -0700

    Expand number range to 256. All ascii characters are allowed in input

--- casil.c
+++ casil.c
@@ -22,11 +22,13 @@ int main( void )
 
 	fclose( fp );
 
+	int n = c;
+
 	fp = fopen( "exe.out", "w" );
 
 	fwrite( start, 31, sizeof( char ), fp);
 	fwrite( move1, 9, sizeof( char ), fp);
-	fputc( c, fp);
+	fprintf( fp, "%d", n );
 	fwrite( move11, 7, sizeof( char ), fp);
 	fwrite( move2, 17, sizeof( char ), fp);
 	fwrite( intrp, 14, sizeof( char ), fp);
